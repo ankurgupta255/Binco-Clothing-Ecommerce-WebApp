ce150261291f5deda0f2fc07e1b18ca85fca13df
Ankur Gupta <gupta.ankur255@gmail.com>
2019-09-10 18:50:01 +0530

    Updated Shop Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop.js';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
-const HatsPage=()=>{
-  return(
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-}
+// const HatsPage=()=>{
+//   return(
+//     <div>
+//       <h1>HATS PAGE</h1>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
