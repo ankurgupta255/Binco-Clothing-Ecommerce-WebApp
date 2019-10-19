c593966fdb6a7af45dc73afe8ed99a22ee8ca541
Ankur Gupta <gupta.ankur255@gmail.com>
2019-10-19 13:27:01 +0530

    Added Cart Feature

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './button.scss';
 
-const Button=({children, ...otherProps})=>{
+const Button=({children, inverted, ...otherProps})=>{
   return(
-    <button className='custom-button' {...otherProps}>
+    <button className={`${inverted ? 'inverted': ''} custom-button`} {...otherProps}>
     {children}
     </button>
   )
--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import Button from '../button/button';
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
 
 import './collection-item.scss';
 
-const CollectionItem=({id , name, price, imageUrl})=>{
+const CollectionItem=({item, addItem})=>{
   return(
     <div className='collection-item'>
-      <div className='image' style={{backgroundImage: `url(${imageUrl})`}}>
+      <div className='image' style={{backgroundImage: `url(${item.imageUrl})`}}>
         <div className='collection-footer'>
-          <span className='name'>{name}</span>
-          <span className='price'>{price}</span>
+          <span className='name'>{item.name}</span>
+          <span className='price'>{item.price}</span>
         </div>
+        <Button onClick={()=>addItem(item)} inverted> Add to Cart</Button>
       </div>
     </div>
   );
 }
 
-export default CollectionItem;
+const mapDispatchToProps=dispatch=>({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
